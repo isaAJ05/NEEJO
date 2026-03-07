772ee82414b0d6ccf3ea4840996e85a887d1e67c
Canela <carpinteron@uninorte.edu.co>
2026-03-07 18:47:58 -0500

    ordenporhoras

--- apps/backend/src/modules/contratacion/domain/factories/orden-servicio.factory.ts
+++ apps/backend/src/modules/contratacion/domain/factories/orden-servicio.factory.ts
@@ -6,7 +6,8 @@ export interface IOrdenServicio {
   tipoServicio: TipoServicio;
   montoTotal: number;
   duracionEstimadaHoras?: number;
-  
+  calcularCostoFinal(): number;
+  validarDatosEspecificos(): boolean;
 }
 
 // Clase base abstracta
@@ -21,6 +22,29 @@ export abstract class OrdenServicioBase implements IOrdenServicio {
     this.montoTotal = montoTotal;
   }
 
-  
+  abstract calcularCostoFinal(): number;
+  abstract validarDatosEspecificos(): boolean;
 }
 
+
+// Implementación concreta: Orden por Horas
+export class OrdenPorHoras extends OrdenServicioBase {
+  tarifaPorHora: number;
+  horasEstimadas: number;
+
+  constructor(tarifaPorHora: number, horasEstimadas: number) {
+    super(TipoServicio.POR_HORAS, tarifaPorHora * horasEstimadas);
+    this.tarifaPorHora = tarifaPorHora;
+    this.horasEstimadas = horasEstimadas;
+    this.duracionEstimadaHoras = horasEstimadas;
+  }
+
+  calcularCostoFinal(): number {
+    // Incluye un 10% de margen para imprevistos
+    return this.montoTotal * 1.10;
+  }
+
+  validarDatosEspecificos(): boolean {
+    return this.tarifaPorHora > 0 && this.horasEstimadas > 0;
+  }
+}
